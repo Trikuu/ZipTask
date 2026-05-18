709ff068d3b762215807eefac8b30ac39130ae72
Trikuu <chaitanyarohan2006@gmail.com>
2026-05-18 12:45:50 +0530

    show full error

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
 
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+try:
+    app = create_app()
+    app.run(host="0.0.0.0", port=10000)
+except Exception as e:
+    import traceback
+    traceback.print_exc()
+    raise
